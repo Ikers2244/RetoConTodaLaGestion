2e85399347ecef32aa552b40b093b49292202fa6
Ikers2244 <115066673+Ikers2244@users.noreply.github.com>
2022-11-13 15:44:25 +0100

    Add files via upload

---
+++ Reto v1/Reto v1/Conexion.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Data.SqlClient
+Imports System.Windows.Controls
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Conexion
+
+    Public MiDataAdapter, MiDataAdapter2 As SqlDataAdapter
+    Public MiDataSet As DataSet
+    Public MiConexion As New SqlConnection
+    Public CadenaConexion = "SERVER=PC-FIRE\SQLEXPRESS; INTEGRATED Security=SSPI;DATABASE=reto"
+
+    Public Sub Conectar()
+        Try
+            MiConexion.ConnectionString = CadenaConexion
+            MiDataAdapter = New SqlDataAdapter("Select * From Articulos WHERE Eliminado= 'False'", MiConexion)
+            MiDataAdapter2 = New SqlDataAdapter("Select * From Empleados WHERE Eliminado= 'False'", MiConexion)
+
+            Dim MicommBuilder As SqlCommandBuilder = New SqlCommandBuilder(MiDataAdapter)
+            Dim MicommBuilder2 As SqlCommandBuilder = New SqlCommandBuilder(MiDataAdapter2)
+
+            MiDataSet = New DataSet
+
+            MiConexion.Open()
+            MiDataAdapter.Fill(MiDataSet, "Articulos")
+            MiDataAdapter2.Fill(MiDataSet, "Empleados")
+            MiConexion.Close()
+
+        Catch ex As Exception
+            MsgBox("Error al crear la conexion:" & vbCrLf & ex.Message)
+            Exit Sub
+        End Try
+    End Sub
+
+    Public Sub CargarDatosArticulos()
+        Dim MiDataRow As DataRow
+        MiDataRow = MiDataSet.Tables("Articulos").Rows(0)
+        FormArticulos.txtNombre.Text = MiDataRow("Nombre")
+        FormArticulos.txtPrecio.Text = MiDataRow("Precio")
+        FormArticulos.txtStock.Text = MiDataRow("Stock")
+        FormArticulos.cbTipo.Text = MiDataRow("Tipo")
+    End Sub
+
+    Public Sub CargarDatosEmpleados()
+        Dim MiDataRow As DataRow
+        MiDataRow = MiDataSet.Tables("Empleados").Rows(0)
+        FormEmpleados.txtNombre.Text = MiDataRow("Nombre")
+        FormEmpleados.txtPswd.Text = MiDataRow("Contraseña")
+        FormEmpleados.txtSueldo.Text = MiDataRow("Sueldo")
+        FormEmpleados.cbAdmin.Text = MiDataRow("Admin")
+        FormEmpleados.txtPuesto.Text = MiDataRow("Puesto")
+    End Sub
+
+    Public Sub ActualizarDgvArticulos()
+        Dim dtTabla As New DataTable
+        Dim sql = "SELECT * FROM Articulos"
+
+        Try
+            Dim conectorSQL = New SqlClient.SqlConnection(CadenaConexion)
+            conectorSQL.Open()
+            Dim comando = New SqlClient.SqlCommand(sql, conectorSQL)
+
+            Dim adaptador = New SqlClient.SqlDataAdapter(comando)
+            MiDataAdapter.Fill(dtTabla)
+            FormArticulos.dgvLista.DataSource = dtTabla
+
+            conectorSQL.Close()
+        Catch ex As Exception
+            MsgBox("ERROR AL CONECTAR")
+        End Try
+    End Sub
+
+    Public Sub ActualizarDgvEmpleados()
+        Dim dtTabla As New DataTable
+        Dim sql = "SELECT * FROM Empleados"
+
+        Try
+            Dim conectorSQL = New SqlClient.SqlConnection(CadenaConexion)
+            conectorSQL.Open()
+            Dim comando = New SqlClient.SqlCommand(sql, conectorSQL)
+
+            Dim adaptador = New SqlClient.SqlDataAdapter(comando)
+            MiDataAdapter2.Fill(dtTabla)
+            FormEmpleados.dgvLista.DataSource = dtTabla
+
+            conectorSQL.Close()
+        Catch ex As Exception
+            MsgBox("ERROR AL CONECTAR")
+        End Try
+    End Sub
+
+End Class
---
+++ Reto v1/Reto v1/Forms/FormPedidos.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormPedidos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1339, 481)
+        Me.Panel1.TabIndex = 0
+        '
+        'FormPedidos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1339, 481)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Century Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "FormPedidos"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+End Class
